1826360e915fc94cde46baad6b5e17b9be6cddf0
milenaa052 <milenasantosdeoliveira40@gmail.com>
2026-04-18 12:43:05 -0300

    feat: creating first files

---
+++ adminService/src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: '.env',
+        }),
+        SequelizeModule.forRoot({
+        dialect: 'mysql',
+        host: process.env.DB_HOST || 'localhost',
+        port:  Number(process.env.DB_PORT) || 3306,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        autoLoadModels: true,
+        synchronize: true,
+        logging: false,
+        }),
+    ],
+    controllers: [AppController],
+    providers: [AppService],
+})
+export class AppModule {}
---
+++ adminService/src/main.ts
@@ -0,0 +1,32 @@
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { join } from 'path';
+import { MulterExceptionFilter } from './common/filters/multer-exception.filter';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.enableCors({
+    origin: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    credentials: true,
+  });
+
+  const uploadDir = process.env.UPLOAD_DIR || 'uploads';
+  app.useStaticAssets(join(__dirname, '..', uploadDir), {
+    prefix: '/uploads',
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+  app.useGlobalFilters(new MulterExceptionFilter());
+  await app.listen(3000, '0.0.0.0');
+}
+
+bootstrap();
