7cee58b1d14fc0ba2c0d903543c259842af0107b
milenaa052 <milenasantosdeoliveira40@gmail.com>
2026-06-20 11:46:37 -0300

    feat: creating password recovery classes

--- adminService/src/admin/admin.controller.ts
+++ adminService/src/admin/admin.controller.ts
@@ -4,10 +4,15 @@ import { CreateAdminDto } from './dto/create-admin.dto'
 import { UpdateAdminDto } from './dto/update-admin.dto'
 import { AuthGuard } from '@nestjs/passport'
 import { AuthRequest } from '../auth/interface/auth-request.interface'
+import { AdminRecoverPassService } from './admin-recover-pass.service'
+import { ResetPasswordDto } from './dto/reset-pass.dto'
 
 @Controller('admin')
 export class AdminController {
-    constructor(private readonly adminService: AdminService) {}
+    constructor(
+        private readonly adminService: AdminService,
+        private readonly adminRecoverPassService: AdminRecoverPassService
+    ) {}
 
     @Post()
     async create(@Body() createAdminDto: CreateAdminDto) {
@@ -35,5 +40,23 @@ export class AdminController {
     ) {
         const loggedAdmin = req.user.idUser
         return this.adminService.update(id, loggedAdmin, updateAdminDto)
+    }
+
+    @Post('recover-password')
+    async recoverPassword(@Body('email') email: string) {
+        await this.adminRecoverPassService.recoverPassword(email)
+
+        return {
+            message: 'Código de recuperação enviado para o e-mail informado'
+        }
+    }
+
+    @Post('reset-password')
+    async resetPassword(@Body() resetPasswordDto: ResetPasswordDto) {
+        await this.adminRecoverPassService.resetPassword(resetPasswordDto)
+
+        return {
+            message: 'Senha redefinida com sucesso'
+        }
     }
 }
